38be3dea1b8894a7ac9829d3cc6a5350cc1c5afb
Jae Chung <jae.chung99@gmail.com>
2018-09-22 15:19:23 +0100

    contract structure

--- recovery-backend/contracts/recovery/src/recovery.cpp
+++ recovery-backend/contracts/recovery/src/recovery.cpp
@@ -1,2 +1,22 @@
 #include "recovery.hpp"
 
+namespace recovery{
+    recovery_contract::recovery_contract(action_name self) : contract(self) {}
+
+    //@abi action
+    void addrecovery(){
+        
+    }
+
+    //@abi action
+    void rmvrecovery(){
+
+    }
+
+    //@abi action
+    void recover(){
+
+    }
+}
+
+EOSIO_ABI( recovery::recovery_contract, (addrecovery)(rmvrecovery)(recover))
--- recovery-backend/contracts/recovery/src/recovery.hpp
+++ recovery-backend/contracts/recovery/src/recovery.hpp
@@ -9,3 +9,31 @@
 
 #include <string>
 #include <vector>
+
+namespace recovery {
+    using std::string;
+    using std::vector;
+
+    using eosio::asset;
+
+    class recovery_contract : public eosio::contract{
+        public:
+            explicit recovery_contract(action_name self);
+
+            //@abi action
+            void addrecovery(){
+
+            }
+
+            //@abi action
+            void rmvrecovery(){
+
+            }
+
+            //@abi action
+            void recover(){
+
+            }
+
+    };
+}//recovery
