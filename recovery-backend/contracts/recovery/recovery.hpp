013d3b896d87fd3b47816bd1ee800730509b2ab8
Jae Chung <jae.chung99@gmail.com>
2018-09-22 14:59:53 +0100

    setup

---
+++ recovery-backend/contracts/recovery/recovery.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <eosiolib/types.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/multi_index.hpp>
+#include <eosiolib/singleton.hpp>
+#include <eosiolib/contract.hpp>
+#include <eosiolib/asset.hpp>
+
+#include <string>
+#include <vector>
---
+++ recovery-frontend/README.md
@@ -0,0 +1,1 @@
+# recovert-frontend
