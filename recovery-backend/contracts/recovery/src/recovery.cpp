953b6c14bab3d4974c82e5af3952dd59c69a458c
Jae Chung <jae.chung99@gmail.com>
2018-09-22 18:22:02 +0100

    recovery logic

--- recovery-backend/contracts/recovery/src/recovery.cpp
+++ recovery-backend/contracts/recovery/src/recovery.cpp
@@ -8,8 +8,9 @@ namespace recovery{
     }
 
     //@abi action
-    void addrecovery(account_name owner, const vector<account_name>& backups){
+    void addrecovery(account_name owner, const vector<account_name>& backups, const string& cell_hash){
         require_auth(owner);
+        eosio_assert(backups.size()>=2, "There should be at least 2 recovery accounts set up.");
 
         recovery_table recovery_accounts(_self, _self);
 
@@ -23,18 +24,88 @@ namespace recovery{
         recovery_accounts.emplace(owner, [&](auto& recovery_info){
            recovery_info.owner = owner;
            recovery_info.backups = backups;
+           recovery_info.cell_hash = cell_hash;
+           recovery_info.rmv_recovery_start_time = 0;
+           recovery_info.recover_start_time = 0;
         });
     }
 
     //@abi action
-    void rmvrecovery(){
-
+    void rmvrecovery(account_name owner){
+        require_auth(owner);
     }
 
     //@abi action
-    void recover(){
+    void recover(account_name owner, account_name requester, const public_key& new_key){
+        require_auth(requester);
+        eosio_assert(is_account(owner));
+        eosio_assert( producer_key != eosio::public_key(), "Public key should not be the default value" );
 
+        recovery_table recovery_accounts(_self, _self);
+        auto itr = recovery_accounts.find(owner);
+        eosio_assert(itr!=recovery_accounts.end(), "Account does not have recovery set up.");
+
+        auto itr_backup = std::find((*itr).backups.begin(), (*itr).backups.end(), requester);
+        eosio_assert(itr_backup != (*itr).backups.end(), "Account is not registered as one of the fallbacks.");
+
+        in_recovery_table in_recovery(_self, _self);
+        auto itr_account = in_recovery.find(owner);
+        eosio_assert(itr_account == in_recovery.end(), "Account is already in the recovery process.");
+
+        auto current_time = now();
+
+        in_recovery.emplace(requester, [&]auto& in_recovery_info){
+            in_recovery_info.owner = owner;
+            in_recovery_info.backups = (*itr).backups;
+            in_recovery_info.cell_hash = (*itr).cell_hash;
+            in_recovery_info.recover_start_time = current_time;
+            in_recovery_info.signed_recovery = vector<account_name> signed_recovery{requester};
+            in_recovery_info.declined_recovery = vector<account_name> declined_recovery;
+        });
     }
+
+    //@abi action
+    void reviewrec(account_name owner, account_name recoverer, bool agree){
+        require_auth(recoverer);
+        eosio_assert(is_accounts(owner));
+
+        in_recovery_table in_recovery(_self, _self);
+        auto itr = in_recovery.find(owner);
+        eosio_assert(itr != in_recovery.end(), "Account is not in the recovery process");
+
+        auto itr_backup = std::find((*itr).backups.begin(), (*itr).backups.end(), recoverer);
+        eosio_assert(itr_backup != (*itr).backups.end(), "Account is not registered as one of the fallbacks.");
+
+        auto current_time = now();
+
+        uint64_t time_since_recover_start = current_time - (*itr).recover_start_time;
+
+        if(time_since_recover_start >= seconds_per_day){
+            in_recovery.erase(itr);
+        }
+        else{
+            if(agree) {
+                in_recovery.modify(itr, 0, [&](auto &in_recovery_info) {
+                    in_recovery_info.signed_recovery = (*itr).signed_recovery.push_back(recoverer);
+                });
+                if((*itr).signed_recovery.size() >= (((*itr).backups.size() * 2) / 3) + 1){
+                    /*
+                     TO-DO UPDATE AUTH
+                     */
+                    in_recovery.erase(itr);
+                }
+            }
+            else{
+                in_recovery.modify(itr, 0, [&](auto &in_recovery_info) {
+                    in_recovery_info.signed_recovery = (*itr).declined_recovery.push_back(recoverer);
+                });
+                if((*itr).declined_recovery.size() >= (((*itr).backups.size() * 2) / 3) + 1){
+                    in_recovery.erase(itr);
+                }
+            }
+        }
+    }
+
 }
 
 EOSIO_ABI( recovery::recovery_contract, (addrecovery)(rmvrecovery)(recover))
--- recovery-backend/contracts/recovery/src/recovery.hpp
+++ recovery-backend/contracts/recovery/src/recovery.hpp
@@ -6,23 +6,43 @@
 #include <eosiolib/singleton.hpp>
 #include <eosiolib/contract.hpp>
 #include <eosiolib/asset.hpp>
+#include <eosiolib/public_key.hpp>
 
 #include <string>
 #include <vector>
+#include <algorithm>
 
 namespace recovery {
     using std::string;
     using std::vector;
     using std::set;
 
+    using eosio::public_key;
     using eosio::asset;
+
+    const uint32_t seconds_per_day = 60 * 60 * 24;
 
     //@abi table
     struct recovery_info{
         account_name owner;
         vector<account_name> backups;
+        string cell_hash;
+        //uint64_t rmv_recovery_start_time;
+        //uint64_t recover_start_time;
         uint64_t primary_key() const { return owner; }
-        EOSLIB_SERIALIZE( recovery_info, (owner)(backups) )
+        EOSLIB_SERIALIZE( recovery_info, (owner)(backups)(cell_hash)/*(rmv_recovery_start_time)(recover_start_time)*/ )
+    };
+
+    //@abi table
+    struct in_recovery_info{
+        account_name owner;
+        vector<account_name> backups;
+        string cell_hash;
+        uint64_t recover_start_time;
+        vector<account_name> signed_recovery;
+        vector<account_name> declined_recovery;
+        uint64_t primary_key() const { return owner; }
+        EOSLIB_SERIALIZE( in_recovery_info, (owner)(backups)(cell_hash)(recover_start_time)(signed_recovery)(decline_recovery))
     };
 
     //@abi table
@@ -32,6 +52,7 @@ namespace recovery {
     };
 
     typedef eosio::multi_index< N(recoverinfo), recovery_info > recovery_table;
+    typedef eosio::multi_index< N(inrecovery), in_recovery_info > in_recovery_table;
     typedef eosio::singleton< N(recoveryenv), recovery_env > recovery_env_singleton;
 
     class recovery_contract : public eosio::contract{
@@ -39,15 +60,15 @@ namespace recovery {
             explicit recovery_contract(action_name self);
 
             //@abi action
-            void addrecovery(){
+            void addrecovery(account_name owner, const vector<account_name>& backups, const string& cell_hash){
             }
 
             //@abi action
-            void rmvrecovery(){
+            void rmvrecovery(account_name owner){
             }
 
             //@abi action
-            void recover(){
+            void recover(account_name owner, account_name requester, const public_key& new_key){
             }
 
         private: recovery_env_singleton m_recovery_env;
