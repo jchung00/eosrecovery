a030f5ef9ffacd7f32fa236f3b3c996b94f2342f
Jae Chung <jae.chung99@gmail.com>
2018-09-22 23:39:25 +0100

    itr_account instead of itr

--- recovery-backend/contracts/recovery/src/recovery.cpp
+++ recovery-backend/contracts/recovery/src/recovery.cpp
@@ -125,19 +125,19 @@ namespace recovery{
         }
 
         else{
-            uint64_t time_since_recover_start = current_time - (*itr).recover_start_time;
+            uint64_t time_since_recover_start = current_time - (*itr_account).recover_start_time;
 
             eosio_assert(new_key == (*itr_account).new_key, "Proposed public key is not the same.");
 
             if(time_since_recover_start >= seconds_per_day){
-                in_recovery.erase(itr);
+                in_recovery.erase(itr_account);
             }
             else{
                 if(agree) {
-                    in_recovery.modify(itr, 0, [&](auto& in_recovery_info) {
-                        in_recovery_info.signed_recovery = (*itr).signed_recovery.push_back(recoverer);
+                    in_recovery.modify(itr_account, 0, [&](auto& in_recovery_info) {
+                        in_recovery_info.signed_recovery = (*itr_account).signed_recovery.push_back(recoverer);
                     });
-                    if((*itr).signed_recovery.size() >= (((*itr).backups.size() * 2) / 3) + 1){
+                    if((*itr_account).signed_recovery.size() >= (((*itr_account).backups.size() * 2) / 3) + 1){
                         authority auth{ 1, {newKey, 1}, {} };
                         eosio::action(eosio::permission_level{_self, N(active)}, N(eosio), N(updateauth),
                         std::make_tuple( owner, active, owner, auth)).send();
@@ -146,10 +146,10 @@ namespace recovery{
                 }
                 else{
                     in_recovery.modify(itr, 0, [&](auto& in_recovery_info) {
-                        in_recovery_info.signed_recovery = (*itr).declined_recovery.push_back(recoverer);
+                        in_recovery_info.signed_recovery = (*itr_account).declined_recovery.push_back(recoverer);
                     });
-                    if((*itr).declined_recovery.size() >= (((*itr).backups.size() * 2) / 3) + 1){
-                        in_recovery.erase(itr);
+                    if((*itr_account).declined_recovery.size() >= (((*itr_account).backups.size() * 2) / 3) + 1){
+                        in_recovery.erase(itr_account);
                     }
                 }
             }
