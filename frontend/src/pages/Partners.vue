3462cb38dee21cd1de57930b46e1c5336a722797
Hugo <hg.campanella@gmail.com>
2018-09-23 05:47:05 +0100

    Partners button changes

--- frontend/src/pages/Partners.vue
+++ frontend/src/pages/Partners.vue
@@ -15,54 +15,61 @@
             <div class="table-responsive">
 
                 <table class="table">
-                    <thead class="thead">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Username</th>
-                        <th scope="col">Actions</th>
-                    </tr>
+                    <thead class="thead" id="custom-head">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Actions</th>
+                        </tr>
                     </thead>
 
                     <tbody>
-                    <tr v-for="(row, index) in rows" :key="index">
-                        <td class="align-middle">{{ index + 1 }}</td>
-                        <td class="align-middle">{{ row.username }}</td>
-                        <td class="align-middle">
-                            <div v-if="row.toDelete">
-                                Deleted
+                        <tr v-for="(row, index) in rows" :key="index">
+                            <td class="align-middle">{{ index + 1 }}</td>
+                            <td class="align-middle">{{ row.username }}</td>
+                            <td class="align-middle">
+                                <div v-if="row.toDelete">
+                                    Deleted
+                                    <b-button variant="danger"
+                                              class="mb-2"
+                                    >
+                                    Undo
+                                    </b-button>
+                                </div>
+
                                 <b-button variant="danger"
                                           class="mb-2"
-                                >
-                                Undo
+                                          @click="deletePartner(index)"
+                                          v-else
+                                          id="custom-button">
+                                    Delete Partner
                                 </b-button>
-                            </div>
+                            </td>
+                        </tr>
 
-                            <b-button variant="danger"
-                                      class="mb-2"
-                                      @click="deletePartner(index)"
-                                      v-else
-                                      id="custom-button">
-                                Delete Partner
-                            </b-button>
-                        </td>
-                    </tr>
+                        <tr>
+                            <td>{{ rows.length + 1 }}</td>
+                            <td class="text-center">
+                                <b-form-input v-model="newUsername"
+                                              type="text"
+                                              placeholder="Enter Username"
+                                              required
+                                              id="custom-input"
+                                >
+                                </b-form-input>
 
-                    <tr>
-                        <td>{{ rows.length + 1 }}</td>
-                        <td>
-                            <input v-model="newUsername" required/>
-                        </td>
-                        <td>
-                            <b-button
-                                    variant="success"
-                                    class="mb-2"
-                                    @click="addPartner"
-                                    id="custom-button"
-                            >
-                                Add Partner
-                            </b-button>
-                        </td>
-                    </tr>
+                            </td>
+                            <td>
+                                <b-button
+                                        variant="secondary"
+                                        class="mb-2"
+                                        @click="addPartner"
+                                        id="custom-button"
+                                >
+                                    Add Partner
+                                </b-button>
+                            </td>
+                        </tr>
                     </tbody>
                 </table>
             </div>
@@ -75,7 +82,6 @@
                 Save recovery partners
             </b-button>
         </div>
-
     </main>
 </template>
 
@@ -114,25 +120,15 @@ export default {
 </script>
 
 <style>
-    html, body {
-        height: 100%;
-    }
-    .main {
-        margin: 0 auto;
-        height: 100%;
-        padding: 20px 0;
-
-        -moz-box-sizing: border-box;
-        -webkit-box-sizing: border-box;
-        box-sizing: border-box;
-    }
-    .footer {
-        background: red;
-        height: 20px;
-    }
 
     #custom-button {
         border-radius: 25px;
+    }
+
+    #custom-input {
+        width: 50%;
+        margin-left: auto;
+        margin-right: auto;
     }
 
 </style>
