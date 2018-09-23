ef98a43f3b6b64f0ba87b4a178d752076243ffb2
Syed Jafri <syed.jafri2@ucalgary.ca>
2018-09-23 04:52:35 +0100

    Update recovery.hpp

--- recovery-backend/contracts/recovery/include/recovery/recovery.hpp
+++ recovery-backend/contracts/recovery/include/recovery/recovery.hpp
@@ -1,37 +1,107 @@
-/**
- *  @file
- *  @copyright defined in eos/LICENSE.txt
- */
 #pragma once
 
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/types.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/multi_index.hpp>
+#include <eosiolib/singleton.hpp>
+#include <eosiolib/contract.hpp>
+#include <eosiolib/asset.hpp>
+#include <eosiolib/public_key.hpp>
+#include <eosiolib/transaction.hpp>
+
 #include <string>
+#include <vector>
+#include <algorithm>
 
-namespace eosio {
+namespace recovery {
+    using std::string;
+    using std::vector;
+    using std::set;
 
-   using std::string;
+    using eosio::public_key;
+    using eosio::asset;
+    using eosio::transaction;
+    using eosio::permission_level;
 
-   class gravatarcafe : public contract {
-      public:
-       gravatarcafe( account_name self ) : contract(self) {}
+    const uint32_t seconds_per_day = 60 * 60 * 24;
 
-         void addgravatar( const account_name account_name,
-                           const string display_name,
-                           const string image_url,
-                           const string telegram    );
 
-         void rmvgravatar( const account_name account_name);
 
-      private:
-         struct gravatar {
-            account_name account_name;
-            string       display_name;
-            string       image_url;
-            string       telegram;
 
-            uint64_t primary_key()const { return account_name; }
-         };
+    struct permission_level_weight {
+        permission_level  permission;
+        weight_type       weight;
+    };
 
-         typedef eosio::multi_index<N(gravatars), gravatar> gravatars;
-   };
-} /// namespace eosio
+    struct key_weight {
+        public_key key;
+        weight_type     weight;
+    };
+
+    struct wait_weight {
+        uint32_t     wait_sec;
+        weight_type  weight;
+    };
+    struct authority {
+        uint32_t                          threshold = 0;
+        vector<key_weight>                keys;
+        vector<permission_level_weight>   accounts;
+        vector<wait_weight>               waits;
+    };
+
+
+
+
+    //@abi table
+    struct recovery_info{
+        account_name owner;
+        vector<account_name> backups;
+        uint64_t last_set_time;
+        uint64_t primary_key() const { return owner; }
+        EOSLIB_SERIALIZE( recovery_info, (owner)(backups)(last_set_time))
+    };
+
+    //@abi table
+    struct in_recovery_inf{
+        account_name owner;
+        vector<account_name> backups;
+        public_key new_key;
+        uint64_t recover_start_time;
+        vector<account_name> signed_recovery;
+        vector<account_name> declined_recovery;
+        uint64_t primary_key() const { return owner; }
+        EOSLIB_SERIALIZE( in_recovery_inf, (owner)(backups)(new_key)(recover_start_time)(signed_recovery)(declined_recovery))
+    };
+
+
+    //@abi table
+    struct recovery_env {
+        uint64_t set_recovery_delay_time = 10;
+        EOSLIB_SERIALIZE( recovery_env, (set_recovery_delay_time));
+    };
+
+    typedef eosio::multi_index< N(recoverinfo), recovery_info > recovery_table;
+    typedef eosio::multi_index< N(inrecovery), in_recovery_inf > in_recovery_table;
+    //typedef eosio::multi_index< N(inremoval), in_removal_info > in_removal_table;
+    typedef eosio::singleton< N(recoveryenv), recovery_env > recovery_env_singleton;
+
+    class recovery_contract : public eosio::contract{
+    public:
+        explicit recovery_contract(action_name self);
+
+        //@abi action
+        void setenv(uint32_t set_recovery_delay_time);
+
+        //@abi action
+        void setrecovery(account_name owner, const vector<account_name>& backups, const string& cell_hash);
+
+        //@abi action
+        void chgrecovery(account_name owner, const vector<account_name>& backups);
+
+        //@abi action
+        void recover(account_name owner, account_name recoverer, const public_key& new_key, bool agree);
+
+    private: recovery_env_singleton m_recovery_env;
+    };
+}//recovery
